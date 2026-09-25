62c3109084eebd7af2b91c1683db46238cbb702d
Kom <komarudingalasta@gmail.com>
2026-09-25 11:24:31 +0700

    PakKom Studio: update firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Firebase Web App configuration for PAKKOM-QUIZBUZZ
+// Security is enforced by Firebase Authentication + Realtime Database Rules.
+export const firebaseConfig = {
+  apiKey: "AIzaSyCed49KeopnEPCrTvWcCs5jX_sb-unB-Uk",
+  authDomain: "pakkom-quizbuzz.firebaseapp.com",
+  databaseURL: "https://pakkom-quizbuzz-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "pakkom-quizbuzz",
+  storageBucket: "pakkom-quizbuzz.firebasestorage.app",
+  messagingSenderId: "482667753620",
+  appId: "1:482667753620:web:b41544f5d4905029bec481"
+};
